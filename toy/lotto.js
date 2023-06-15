774c8d30e56f60f79a6d524b5907bd5a7c39c62e
kimjongwan2209 <aegis0918@naver.com>
2023-06-15 21:16:18 +0900

    lotto.js refactoring

--- toy/lotto.js
+++ toy/lotto.js
@@ -1,20 +1,27 @@
 const randomNum = () => {
   const array = [];
+  const colorName = [];
+
   while (array.length < 6) {
     const num = parseInt(Math.floor(Math.random() * 45 + 1));
 
     if (array.indexOf(num) === -1) {
       array.push(num);
     }
+
+    const randomColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+
+    if (colorName.indexOf(randomColor) === -1) {
+      colorName.push(randomColor);
+    }
   }
   array.sort((a, b) => a - b);
 
-  const randomColor1 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const randomColor2 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const randomColor3 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const randomColor4 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const randomColor5 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const randomColor6 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  //   const randomColor2 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  //   const randomColor3 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  //   const randomColor4 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  //   const randomColor5 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  //   const randomColor6 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
 
   document.getElementById("num1").innerHTML = array[0];
   document.getElementById("num2").innerHTML = array[1];
@@ -23,10 +30,10 @@ const randomNum = () => {
   document.getElementById("num5").innerHTML = array[4];
   document.getElementById("num6").innerHTML = array[5];
 
-  document.getElementById("num1").style.color = randomColor1;
-  document.getElementById("num2").style.color = randomColor2;
-  document.getElementById("num3").style.color = randomColor3;
-  document.getElementById("num4").style.color = randomColor4;
-  document.getElementById("num5").style.color = randomColor5;
-  document.getElementById("num6").style.color = randomColor6;
+  document.getElementById("num1").style.color = colorName[0];
+  document.getElementById("num2").style.color = colorName[1];
+  document.getElementById("num3").style.color = colorName[2];
+  document.getElementById("num4").style.color = colorName[3];
+  document.getElementById("num5").style.color = colorName[4];
+  document.getElementById("num6").style.color = colorName[5];
 };
